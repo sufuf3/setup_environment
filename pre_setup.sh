671e1edc67c6842dec936fc28b030f7442a0900e
Samina Fu <sufuf3@gmail.com>
2018-04-27 16:31:09 +0800

    Update pre_setup.sh

--- pre_setup.sh
+++ pre_setup.sh
@@ -18,9 +18,17 @@ sed -E -i "s/([a-z]+.)?archive.ubuntu.com/$UBUNTU_APT_SITE/g" /etc/apt/sources.l
 sed -i "s/security.ubuntu.com/$UBUNTU_APT_SITE/g" /etc/apt/sources.list
 
 apt update
-apt upgrade -y && \
+apt upgrade -y
+
+# Timezone Set
+TZ=Asia/Taipei
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y tzdata
+dpkg-reconfigure --frontend noninteractive tzdata
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
 apt install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
-vim nmap iperf iperf3 tmux traceroute git sshpass curl openssh-server tree htop build-essential bash-completion python-pip python-dev build-essential python-setuptools python-numpy python-scipy python-matplotlib ipython python-setuptools software-properties-common && \
+vim nmap iperf iperf3 tmux traceroute git sshpass curl openssh-server tree htop build-essential bash-completion python-pip python-dev build-essential python-setuptools python-numpy python-scipy python-matplotlib python-setuptools software-properties-common && \
 apt clean
 
 sed -i "s/PermitRootLogin no/PermitRootLogin yes/g" /etc/ssh/sshd_config
@@ -57,14 +65,11 @@ adduser opadmin lpadmin
 adduser opadmin sambashare
 echo "opadmin ALL=NOPASSWD: ALL" >> /etc/sudoers
 
-rm pre_setup_resolve.conf
+rm -f pre_setup_resolve.conf
 
 # Local: install Ansible
 apt-add-repository ppa:ansible/ansible -y
 apt-get update
 apt-get install ansible -y
 
-git clone https://bitbucket.org/itri-opteam/auto-deploy-k8s.git && cd auto-deploy-k8s
-
-echo "Please modify the files: set_env/hosts, auto-deploy.sh, inventory, group_vars/all.yml, according to README.md"
-echo "After all, please execute the auto-deploy.sh script"
+echo "Please download KaaS deploy tool"
